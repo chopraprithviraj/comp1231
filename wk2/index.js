c492b9fab60e7fb40e9c61171bccbdd93c814894
Prithvi Raj Chopra <PrithviRaj.Chopra@icloud.com>
2020-01-15 17:38:24 -0500

    first commit

---
+++ wk2/index.js
@@ -0,0 +1,11 @@
+console.log('it worked!')
+
+var name = 'Bob'
+const SIN = '123456789'  
+let phone = '6479040000'
+
+console.log(
+    `${name} has a SIN that is ${SIN} and his number is ${phone}`
+)
+
+var varl, var2, var3
