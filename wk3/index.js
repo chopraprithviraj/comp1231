4c9737628f3a35278e5d5946611c683f228d4521
Prithvi Raj Chopra <PrithviRaj.Chopra@icloud.com>
2020-01-21 23:08:29 -0500

    second commit

---
+++ wk3/index.js
@@ -0,0 +1,10 @@
+console.log('it works!')
+var favNumber = (9)
+let colour = ('green')
+const myNameisPrithvi = (true)
+console.log (favNumber, colour, myNameisPrithvi)
+
+let course
+let classTime = Math.floor(Math.random() * 100);
+
+console.log('The number is between 50 and 99')
